b191bcdf24389196cdf177f8e428323940a93c2e
slimek <slime573@gmail.com>
2014-08-19 17:22:49 +0800

    (minor) Add SandboxSuite.
    - Test anything as you wish :)

---
+++ test/Sandbox/SandboxSuite.cpp
@@ -0,0 +1,23 @@
+// Macaron C++ Library Test - Sandbox Suite
+
+#include "MacaronTestPch.h"
+
+//
+// Test anything as you with :)
+//
+
+
+namespace Macaron
+{
+
+SUITE( SandboxSuite )
+{
+
+TEST( SandboxTest )
+{
+}
+
+
+} // SUITE SandboxSuite
+
+} // namespace Macaron
